58b97244b07eca5f6a0cc83b294e12572f2ba641
tracer-ics2023 <tracer@njuics.org>
2024-09-05 16:26:09 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:26:09 up 1 day,  8:27,  7 users,  load average: 0.07, 0.02, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -33,9 +33,6 @@ enum {
 	TK_LEFT_BRACKET,
 	TK_RIGHT_BRACKET,
 	TK_NUM,
-
-  /* TODO: Add more token types */
-
 };
 
 static struct rule {
@@ -142,25 +139,73 @@ static bool make_token(char *e) {
   return true;
 }
 
+/*
+ * check_parentheses - Check if the token array is a parenthesized expression.
+ * @param p: The left boundary index of the token array.
+ * @param q: The right boundary index of the token array.
+ 
+ * This function checks if the token array from index 'p' to 'q' (inclusive)
+ * is enclosed by a pair of matching parentheses. If the tokens form a valid
+ * parenthesized expression, the function returns true. Otherwise, it returns false.
+*/
+bool check_parentheses(int p,int q) {
+	// 1. Check if the tokens at positions 'p' and 'q' are '(' and ')' respectively.
+	if(tokens[p].type == TK_LEFT_BRACKET && tokens[q].type == TK_RIGHT_BRACKET) {
+		p = p + 1;
+		q = q - 1;
+	} else {
+		return false;
+	}
+	// 2. For the inner expression (i.e., the tokens between 'p+1' and 'q-1'), 
+	// verify that all parentheses are correctly matched.
+	int left_brackets_num = 0;
+
+	for (int i = p; i < q && left_brackets_num >= 0; i++) {
+		if(tokens[i].type == TK_LEFT_BRACKET) {
+			left_brackets_num += 1;
+		}
+
+		if(tokens[i].type == TK_RIGHT_BRACKET) {
+			left_brackets_num -= 1;
+		}
+	}
+	/*
+	 * If num is greater than 0, it indicates there are extra left parentheses '('
+	 * that cannot be matched with corresponding right parentheses.
+	 * If num is less than 0, it indicates there are extra right parentheses ')'.
+	 */
+	if (left_brackets_num != 0) {
+		return false;
+	}
+	return true;
+}
+
+
 #ifndef TEST
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
-
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
+	
   return 0;
 }
 #endif
 
 #ifdef TEST
+void test_check_parentheses() {
+	tokens[32] = {};
+	char *str = "(123 + 456 +12)";
+	make_token(str);
+	assert(check_parentheses(0, sizeof(str)-1));
+	printf("check ok!\n");
+}
+
 int main() {
 	init_regex();
-	assert(make_token("(123+789-323)"));
+	//assert(make_token("(123+789-323)"));
 	//assert(make_token("(+-*///**++---)"));
+	test_check_parentheses();
 	return 0;
 }
 #endif
