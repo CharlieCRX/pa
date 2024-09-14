27b175932258bfb0c20e01c6f510d02d4496dfa0
tracer-ics2023 <tracer@njuics.org>
2024-09-14 15:41:42 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:41:42 up  7:11,  5 users,  load average: 0.62, 0.30, 0.22

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -100,7 +100,7 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s\n",
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				#ifdef TEST
 				//printf("\n");
