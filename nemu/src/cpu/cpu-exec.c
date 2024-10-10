4e43fbc4d297046b91fadecf03147cd17af93e4f
tracer-ics2023 <tracer@njuics.org>
2024-10-10 14:10:09 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:10:09 up 8 days, 20:50,  6 users,  load average: 0.57, 0.16, 0.05

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -53,9 +53,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
-	//printf("\nbefore:pc = 0x%08x, snpc = 0x%08x, dnpc = 0x%08x\n", s->pc, s->snpc, s->dnpc);
   isa_exec_once(s);
-	//printf("\nAfter:pc = 0x%08x, snpc = 0x%08x, dnpc = 0x%08x\n", s->pc, s->snpc, s->dnpc);
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
