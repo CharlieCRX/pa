db798af2ba7b13d6454dfd562a6f5326f0921213
tracer-ics2023 <tracer@njuics.org>
2024-10-18 23:14:08 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:14:08 up 11 days, 14:33,  9 users,  load average: 0.00, 0.01, 0.06

--- nemu/src/cpu/iringbuf/iringbuf.c
+++ nemu/src/cpu/iringbuf/iringbuf.c
@@ -1,7 +1,6 @@
 #include "iringbuf.h"
 
 void init_ringbuf(iringbuf *rb) {
-	rb = (iringbuf *) malloc(sizeof(iringbuf));
 	rb->head = NULL;
 	rb->tail = NULL;
 	rb->count = 0;
@@ -52,7 +51,6 @@ void destroy_ringbuf(iringbuf *rb) {
 	while( rb->count > 0 ) {
 		pop(rb);
 	}
-	free(rb);
 }
 
 void print_ringbuf(iringbuf *rb) {
