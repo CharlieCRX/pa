ddd99e4943d8a3d0012d3e1005dea6347b6a111c
tracer-ics2023 <tracer@njuics.org>
2024-10-18 23:21:20 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:21:20 up 11 days, 14:40,  9 users,  load average: 0.00, 0.00, 0.03

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -171,9 +171,9 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
+			destroy_ringbuf(rb);
       // fall through
     case NEMU_QUIT: statistic();
   }
 	print_ringbuf(rb);
-	destroy_ringbuf(rb);
 }
