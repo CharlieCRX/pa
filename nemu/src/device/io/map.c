96ded0db01fbbe511e98259fa15b001bf8f1c12b
tracer-ics2023 <tracer@njuics.org>
2024-10-30 18:06:48 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:06:48 up 1 day, 16:14, 10 users,  load average: 0.36, 0.08, 0.03

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -23,7 +23,7 @@
 static uint8_t *io_space = NULL;
 static uint8_t *p_space = NULL;
 
-uint8_t* new_space(int size) {
+uint8_t* new_space(int size) {	// 4096
   uint8_t *p = p_space;
   // page aligned;
   size = (size + (PAGE_SIZE - 1)) & ~PAGE_MASK;
