3a7c7bd4b3a32ca56403713012a88c704aec1b85
tracer-ics2023 <tracer@njuics.org>
2024-10-31 16:51:05 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:51:05 up 2 days,  5:04, 13 users,  load average: 0.04, 0.05, 0.03

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,8 +6,8 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint64_t us = 0;
-	uint32_t us_low  = inl(RTC_ADDR);
 	uint32_t us_high = inl(RTC_ADDR + 4);
+	uint32_t us_low  = inl(RTC_ADDR);
 	us = (uint64_t)us_low | ((uint64_t)us_high << 32);
 	uptime->us = us;
 }
