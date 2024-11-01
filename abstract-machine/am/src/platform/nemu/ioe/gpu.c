78754ae75a1d0a02217c4b84a814117c79281940
chenruixin <rxchen1999@163.com>
2024-11-02 01:51:10 +0800

    __am_gpu_fbdraw update

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,6 +23,16 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   // 若`sync`为`true`, 则马上将帧缓冲中的内容同步到屏幕上
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0)) return;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = BITS(inl(VGACTL_ADDR), 31, 16);
+  for (int i = y; i < y+h; i++) {
+    for (int j = x; j < x+w; j++) {
+      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
