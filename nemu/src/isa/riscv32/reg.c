d7783dbf043a14c99db776ef5581309d389c7306
tracer-ics2023 <tracer@njuics.org>
2024-11-09 16:54:53 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:54:53 up 5 days,  9:09, 12 users,  load average: 0.10, 0.12, 0.13

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -70,6 +70,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
 
 word_t get_csr_val_by_id(int csr_id) {
 	// TODO
+	printf("csr_id = %d\n", csr_id);
 	switch(csr_id) {
 		case CSR_MTVEC: return cpu.csrs.mtvec;
 		default:assert(0);
