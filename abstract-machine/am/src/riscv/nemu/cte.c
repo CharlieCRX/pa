8a59f9b3db2a5b9d1c3198d367213548d4b13a20
tracer-ics2023 <tracer@njuics.org>
2024-11-11 21:50:32 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:50:32 up 6 days,  7:42,  6 users,  load average: 0.00, 0.01, 0.07

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,9 +7,12 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    switch (c->mcause) {
-			case 16:					ev.event = EVENT_YIELD;		break;
-      default:				  ev.event = EVENT_ERROR;   break;
+    if (c->GPR1 == -1) {
+      ev.event = EVENT_YIELD;
+    } else if (c->GPR1 >= 0 && c->GPR1 <= 19) {
+      ev.event = EVENT_SYSCALL;
+    } else {
+      ev.event = EVENT_ERROR;
     }
 
     c = user_handler(ev, c);
