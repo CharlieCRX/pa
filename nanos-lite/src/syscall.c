0442ca94c3233bc51a3bb1bfead2734d10781a05
tracer-ics2023 <tracer@njuics.org>
2024-11-13 17:25:44 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:25:44 up 7 days,  3:53,  3 users,  load average: 0.01, 0.02, 0.07

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,10 +13,8 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case SYS_exit : sys_exit(c);  break;
+    //case SYS_exit : sys_exit(c);  break;
     case SYS_yield: sys_yield(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
-
-
 }
