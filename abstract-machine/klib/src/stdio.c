9419645bca69e6625a265a0fb2ec0b253939790c
tracer-ics2023 <tracer@njuics.org>
2024-11-13 13:44:21 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:44:21 up 7 days, 12 min,  3 users,  load average: 0.78, 0.51, 0.29

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -72,7 +72,6 @@ int process_format_string(char *out, const char *fmt, va_list args) {
         int i = va_arg(args, int);
         char hex_str[20];
         int_to_hex(i, hex_str); // Convert integer to hexadecimal string
-        printf("hex:%s", hex_str);
         strcpy(out_ptr, hex_str); // Copy the hex string to buffer
         out_ptr += strlen(hex_str);
       }
@@ -131,6 +130,7 @@ void int_to_hex(int value, char *out) {
         buffer[index++] = '0';
     } else {
         while (value > 0) {
+          printf("@value = %d,", value);
             buffer[index++] = hex_digits[value & 0xF];
             value >>= 4;
         }
