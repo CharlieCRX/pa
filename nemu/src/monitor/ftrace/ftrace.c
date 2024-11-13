3b55af07eede2781ace605510a0b61e41f603b43
tracer-ics2023 <tracer@njuics.org>
2024-11-13 10:48:49 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:49 up 6 days, 21:16,  3 users,  load average: 0.80, 0.26, 0.16

--- nemu/src/monitor/ftrace/ftrace.c
+++ nemu/src/monitor/ftrace/ftrace.c
@@ -189,8 +189,7 @@ void trace_dwrite(paddr_t addr, int len, word_t data, IOMap *map) {
 // etrace
 /* 记录异常处理的踪迹 */
 void etrace(vaddr_t epc, word_t mcause, word_t gpr, word_t mtvec) {
-  printf("etrace: epc = " FMT_WORD ", mcause = "FMT_WORD", gpr(a7) = " FMT_WORD", mtvec = "FMT_WORD "\n",
-  epc, mcause, gpr, mtvec);
+  IFDEF(CONFIG_TARGET_NATIVE_ELF, printf("\nCONFIG_TARGET_NATIVE_ELF\n"));
   log_write("etrace: epc = " FMT_WORD ", mcause = "FMT_WORD", gpr(a7) = " FMT_WORD", mtvec = "FMT_WORD "\n",
   epc, mcause, gpr, mtvec);
 }
