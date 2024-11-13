76d71a4531c7c52802844a085ee023b5b0f88d7d
tracer-ics2023 <tracer@njuics.org>
2024-11-13 11:05:17 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:05:17 up 6 days, 21:33,  3 users,  load average: 0.25, 0.26, 0.25

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -148,7 +148,7 @@ static int decode_exec(Decode *s) {
 
 	/**interupt**/
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, ECALL(s->dnpc));
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, { s->dnpc = read_csrs(CSR_MEPC); ETRACE("mret");});//TODO
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, { s->dnpc = read_csrs(CSR_MEPC); ETRACE("mret ");});//TODO
 
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
