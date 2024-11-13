a4ebcb3729fb5a718c100297318ade9dfc1b1d5a
tracer-ics2023 <tracer@njuics.org>
2024-11-13 17:39:20 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:39:20 up 7 days,  4:07,  3 users,  load average: 0.04, 0.04, 0.05

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,10 @@
 #include <common.h>
 #include "syscall.h"
 
+void strace(Context *c) {
+  Log("strace: Name: %d, param(a0, a1, a2) = (%d, %d, %d), ret  = %d\n", 
+  c->GPR1, c->GPR2, c->GPR3, c->GPR4, c->GPRx);
+}
 void sys_yield(Context *c) {
   yield();
   c->GPRx = 0;
@@ -17,4 +21,5 @@ void do_syscall(Context *c) {
     case SYS_yield: sys_yield(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  strace(c);
 }
