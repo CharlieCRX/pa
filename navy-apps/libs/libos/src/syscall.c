6ae085d84755000e2f8898a24a7a189896f7d498
tracer-ics2023 <tracer@njuics.org>
2024-11-14 22:12:39 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:12:39 up 7 days, 20:07,  8 users,  load average: 0.00, 0.05, 0.10

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -72,7 +72,7 @@ int _write(int fd, void *buf, size_t count) {
 
 void *_sbrk(intptr_t increment) {
   intptr_t new_address = (intptr_t) &end + increment;
-  return _syscall_(SYS_brk, new_address, 0, 0);
+  return _syscall_(SYS_brk, 0, 0, 0);
 }
 
 int _read(int fd, void *buf, size_t count) {
