332858a4d0ff575a47af846f0c21d5d89ac8e583
tracer-ics2023 <tracer@njuics.org>
2024-11-19 18:57:57 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:57:57 up 9 days, 13:53,  5 users,  load average: 0.06, 0.10, 0.13

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -20,8 +20,9 @@ int main() {
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
+  printf("*****************\n");
+  fseek(fp, 0, SEEK_CUR);
 
-  printf("00000000000000000000000000000\n");
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
