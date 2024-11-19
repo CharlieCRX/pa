25f9f60b5d64413174deee797ffdd4334b06f9ae
tracer-ics2023 <tracer@njuics.org>
2024-11-19 13:45:04 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:45:04 up 9 days,  8:49,  5 users,  load average: 0.10, 0.04, 0.04

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -5,7 +5,6 @@
 #include <time.h>
 #include "syscall.h"
 
-extern char end;
 // helper macros
 #define _concat(x, y) x ## y
 #define concat(x, y) _concat(x, y)
@@ -71,7 +70,8 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  static char *myend = &end;
+  extern char _end;
+  static char *myend = &_end;
   if (_syscall_(SYS_brk, increment, 0, 0) == 0) {
     void *ret = myend;
     myend += increment;
