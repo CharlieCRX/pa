d497200b4d88b9f5890c5ae03d7f4b0d13f7d1e5
tracer-ics2023 <tracer@njuics.org>
2024-11-19 17:03:18 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:18 up 9 days, 12:07,  5 users,  load average: 0.27, 0.12, 0.14

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -29,12 +29,15 @@ void sys_write(Context *c) {
 }
 
 void sys_sbrk(Context *c) {
-  for(int i = 0; i < 100000; i++);
-  c->GPRx = 0;
+  c->GPRx = -1;
 }
 
 void sys_open(Context *c) {
+  const char *pathname = (char *)c->GPR2;
+  int flags = (int) c->GPR3;
+  int mode = (int) c->GPR4;
 
+  c->GPRx = fs_open(pathname, flags, mode);
 }
 
 void sys_read(Context *c) {
