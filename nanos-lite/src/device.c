361f1e654750b32b3b13dd4bfced74d79a32e829
tracer-ics2023 <tracer@njuics.org>
2024-11-20 15:48:04 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:48:04 up 9 days, 19:10,  8 users,  load average: 0.11, 0.15, 0.11

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -16,7 +16,10 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  putstr(buf);
+  const char *char_buf = (const char *)buf;
+	for (int i = 0; i < len; i++) {
+		putch(char_buf[i]);
+	}
   return len;
 }
 
