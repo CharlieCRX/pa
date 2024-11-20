4469109744f33c087463311a97a1d6c77fa01122
tracer-ics2023 <tracer@njuics.org>
2024-11-20 13:12:49 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:12:49 up 9 days, 16:35,  8 users,  load average: 0.12, 0.10, 0.09

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,24 +2,30 @@
 #include <assert.h>
 
 int main() {
+  // 判断num文件是否打开
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
 
+  // 判断文件大小
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
 
+  // 检查文件501到1000行的数据
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+  printf("check 501-1000 lines OK!\n");
 
+  // 修改1到500行的数据
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
+  printf("modify 1-500 lines data OK!\n");
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
