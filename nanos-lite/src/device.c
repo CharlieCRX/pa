069ae30b802dccd8466e0617d18c1234adfb3762
tracer-ics2023 <tracer@njuics.org>
2024-11-21 11:14:58 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:14:58 up 10 days, 34 min,  8 users,  load average: 0.10, 0.23, 0.29

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -47,9 +47,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   const char *event_name    = keyname[ev.keycode];
   char *event_keydown = ev.keydown ? "kd" : "ku";
   sprintf(buf, "%s %s", event_keydown, event_name); 
-  //assert(buf == NULL);
-  Log("Input = %s\n", buf);
-  assert(0);
+  
+  assert(buf != NULL);
   // 获取写入的长度
   return strlen(buf);
 }
