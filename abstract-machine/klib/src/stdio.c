6241798e5968862157daaf7734595410bb478627
tracer-ics2023 <tracer@njuics.org>
2024-11-21 18:04:13 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:04:13 up 10 days,  7:23,  5 users,  load average: 0.05, 0.18, 0.16

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -64,8 +64,9 @@ int process_format_string(char *out, const char *fmt, va_list args) {
 			}
 			else if (*fmt_ptr == 's') {
 				char *s = va_arg(args, char *);
-				strcpy(out_ptr, s);
-				out_ptr += strlen(s);
+				// strcpy(out_ptr, s);
+        putstr(s);
+				out_ptr += 0;
 			}
       else if (*fmt_ptr == 'x' || *fmt_ptr == 'p') {
         // Handle hexadecimal format (%x)
@@ -91,7 +92,7 @@ int printf(const char *fmt, ...) {
 	va_list args;
 	va_start(args, fmt);
 
-  panic_on((strlen(fmt) > MAX_STRING_LEN), "Too lang for printf");
+  panic_on((strlen(fmt) > MAX_STRING_LEN), "Too lang for printf");//废话，不可能超长的
 	int len = process_format_string(buf, fmt, args);
 
 	for (int i = 0; i < len; i++) {
