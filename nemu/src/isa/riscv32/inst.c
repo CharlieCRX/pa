e844c2c9246ce7063d40a5bacf537d9e6603903d
tracer-ics2023 <tracer@njuics.org>
2024-11-25 18:47:54 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:47:54 up 11 days,  2:29,  8 users,  load average: 0.25, 0.20, 0.27

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -26,7 +26,13 @@
 void trace_func_call(paddr_t pc, paddr_t target);
 void trace_func_ret(paddr_t pc);
 void etrace(const char *inst, vaddr_t epc, word_t mcause, word_t gpr, word_t mtvec);
+#define ETRACE(inst) do { bool success = true; word_t a7 = isa_reg_str2val("a7",&success); assert(success);  etrace(inst, read_csrs(CSR_MEPC), read_csrs(CSR_MCAUSE), a7, read_csrs(CSR_MTVEC));} while(0)
+#define ECALL(dnpc) do { dnpc = isa_raise_intr(ENVIRONMENT_CALL_FROM_M_MODE, s->pc); ETRACE("ecall");} while(0)
+#else
+#define ETRACE(inst) do{} while(0);
+#define ECALL(dnpc) do {} while(0);
 #endif
+
 
 
 enum {
@@ -41,8 +47,7 @@ enum {
 #define immB() do { *imm = SEXT(BITS(i, 31, 31), 1) << 12 | BITS(i, 7, 7) << 11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1 | 0; } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immJ() do { *imm = SEXT(((BITS(i, 31, 31) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1) | 0),21);} while(0) 
-#define ETRACE(inst) do { bool success = true; word_t a7 = isa_reg_str2val("a7",&success); assert(success);  etrace(inst, read_csrs(CSR_MEPC), read_csrs(CSR_MCAUSE), a7, read_csrs(CSR_MTVEC));} while(0)
-#define ECALL(dnpc) do { dnpc = isa_raise_intr(ENVIRONMENT_CALL_FROM_M_MODE, s->pc); ETRACE("ecall");} while(0)
+
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
   int rs1 = BITS(i, 19, 15);
