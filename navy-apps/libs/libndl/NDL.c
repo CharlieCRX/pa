a29b695ebcaf8107beacc3f89b5d73363b7da7f5
tracer-ics2023 <tracer@njuics.org>
2024-11-26 17:14:41 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:14:41 up 11 days, 13:53,  8 users,  load average: 0.32, 0.21, 0.19

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -131,7 +131,8 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
 
   int offset;
   uint32_t lines_pixels[405];
-
+  
+  printf("NDL_DrawRect: line start storing...\n");
   // 固定画布高度，将画布的每行存储到显存中
   for (int j = 0; j < h; j++) { 
     // 确定画布每一行的初始像素 在屏幕中的偏移
@@ -258,7 +259,6 @@ void str_to_one_pair(const char *oneKeyValueString, KeyValuePair *pair) {
  * @date 2024-11-26
  */
 void str_to_pairs(const char *mulKeyValueString, KeyValuePair pairs[]) {
-  printf("str_to_pairs:%s", mulKeyValueString);
   char tempStr[500];
   assert(strlen(mulKeyValueString) <= 500);
   strcpy(tempStr, mulKeyValueString);
