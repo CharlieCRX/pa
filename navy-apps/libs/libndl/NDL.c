ae9606cc1a658f0fe2e30b7d6923e6c42f578ae9
tracer-ics2023 <tracer@njuics.org>
2024-11-26 17:19:03 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:19:03 up 11 days, 13:57,  8 users,  load average: 0.48, 0.29, 0.21

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -146,7 +146,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
 
     // 设置显存的偏移为此行第一个像素的位置
     assert(fseek(fp, offset, SEEK_SET) == 0);
-    printf("%d line is ok\n", i);
+    printf("%d line is ok\n", j);
     // 将此行的像素值数组保存到显存中
     size_t num_written = fwrite(lines_pixels, sizeof(uint32_t), w, fp);
     assert(num_written == w);
