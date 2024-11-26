9692d5791ac6babfbacc3bd80b2811ab5310a9d6
tracer-ics2023 <tracer@njuics.org>
2024-11-26 17:09:43 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:43 up 11 days, 13:48,  8 users,  load average: 0.21, 0.14, 0.19

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -61,23 +61,25 @@ int NDL_PollEvent(char *buf, int len) {
  * @date 2024-11-25
  */
 void NDL_GetDisplayInfo(int *width, int *height) {
-  FILE *fp = fopen("/proc/dispinfo", "r");
-  assert(fp);
+  // FILE *fp = fopen("/proc/dispinfo", "r");
+  // assert(fp);
 
-  // 建立两个键值对存放高度和宽度
-  KeyValuePair pairs[2];
+  // // 建立两个键值对存放高度和宽度
+  // KeyValuePair pairs[2];
 
-  // 将键值对字符串解析为结构体数组
-  char line[256];  // 用来存储读取的一行内容
-  if (fgets(line, sizeof(line), fp) != NULL) {
-    printf("sizeof(line) = %d\n", sizeof(line));
-    // fgets 读取到一行内容
-    printf("Read line: %s\n", line);
-  }
+  // // 将键值对字符串解析为结构体数组
+  // char line[256];  // 用来存储读取的一行内容
+  // if (fgets(line, sizeof(line), fp) != NULL) {
+  //   printf("sizeof(line) = %d\n", sizeof(line));
+  //   // fgets 读取到一行内容
+  //   printf("Read line: %s\n", line);
+  // }
 
-  str_to_pairs(line, pairs);
-  *width = get_width(pairs);
-  *height = get_height(pairs);
+  // str_to_pairs(line, pairs);
+  // *width = get_width(pairs);
+  // *height = get_height(pairs);
+  *width = 400;
+  *height = 300;
   printf("NDL_GetDisplayInfo ok!\n");
 }
 
