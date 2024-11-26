aaf6dbfc31a41788f73257240ebdac18ffbfbdd8
tracer-ics2023 <tracer@njuics.org>
2024-11-26 13:39:43 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:39:43 up 11 days, 10:18,  8 users,  load average: 0.31, 0.21, 0.27

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -72,14 +72,13 @@ void NDL_GetDisplayInfo(int *width, int *height) {
   while (fgets(line, sizeof(line), fp) != NULL) {
     // fgets 读取到一行内容
     printf("Read line: %s\n", line);
-    break;
   }
 
-  fclose(fp);  // 关闭文件
-  // str_to_pairs(fp, pairs);
-  // *width = get_width(pairs);
-  // *height = get_height(pairs);
-  // printf("screen size:width = %d, height = %d\n", *width, *height);
+  
+  str_to_pairs(line, pairs);
+  *width = get_width(pairs);
+  *height = get_height(pairs);
+  printf("screen size:width = %d, height = %d\n", *width, *height);
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
