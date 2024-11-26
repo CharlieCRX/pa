74bf2cf0928cdcb45550c7765bf521a54c27a894
tracer-ics2023 <tracer@njuics.org>
2024-11-26 19:40:23 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:40:23 up 11 days, 16:19,  8 users,  load average: 0.91, 1.27, 1.15

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -107,7 +107,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   memcpy(pixels, buf, len);
   
   // 将此行像素点写入到显存中
-  io_write(AM_GPU_FBDRAW, x, y, pixels, len/2, 1, true);
+  io_write(AM_GPU_FBDRAW, x, y, pixels, len/4, 1, true);
 
   return len;
 }
