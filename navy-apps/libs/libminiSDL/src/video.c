1423a39043d2e0f8021a36f5ee6011f7e16f5ee3
tracer-ics2023 <tracer@njuics.org>
2024-12-04 17:15:09 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:15:09 up 14 days,  3:01,  5 users,  load average: 0.08, 0.22, 0.29

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -10,14 +10,16 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 
   // 如果没有提供源矩形，使用整个源表面
   if (srcrect == NULL) {
+    printf("srcrect == NULL\n");
     srcrect = &((SDL_Rect){0, 0, src->w, src->h});
   }
 
   // 如果没有提供目标矩形，使用整个目标表面
   if (dstrect == NULL) {
+      printf("dstrect == NULL\n");
       dstrect = &((SDL_Rect){0, 0, dst->w, dst->h});
   }
-
+  
   printf("screen src w = %d, h = %d\n", src->w, src->h);
   printf("screen dst w = %d, h = %d\n", dst->w, dst->h);
   printf("srcrect w = %d, h = %d\n", srcrect->w, srcrect->h);
