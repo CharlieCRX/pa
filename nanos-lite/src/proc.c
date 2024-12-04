59892b20204037e2dcd8191a865800c315da9a13
tracer-ics2023 <tracer@njuics.org>
2024-12-04 15:35:02 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:35:02 up 14 days,  1:21,  5 users,  load average: 0.01, 0.15, 0.16

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void init_proc() {
 
   // load program here
 	//naive_uload(NULL, "/bin/timer-test");
-  naive_uload(NULL, "bmp-test");
+  naive_uload(NULL, "/bin/menu");
 }
 
 Context* schedule(Context *prev) {
