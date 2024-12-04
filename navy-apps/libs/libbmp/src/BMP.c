79a3f2481b807fb9c888ffd780c63ccab02917a8
tracer-ics2023 <tracer@njuics.org>
2024-12-04 19:20:20 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:20 up 14 days,  5:06,  5 users,  load average: 0.14, 0.49, 0.53

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -43,7 +43,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       uint8_t g = *(((uint8_t*)&pixels[w * i]) + 3 * j + 1);
       uint8_t r = *(((uint8_t*)&pixels[w * i]) + 3 * j + 2);
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
-      printf("pixels[%d] = 0x%x, ", w * i + j, pixels[w * i + j]);
+      // printf("pixels[%d] = 0x%x, ", w * i + j, pixels[w * i + j]);
     }
   }
   fseek(fp, 0, SEEK_SET);
--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -65,7 +65,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
       // 否则正常复制即可
       // dst->pixels[dst_now_idx] = src->pixels[src_now_idx];
       // printf("src->pixels[src_now_idx] = 0x%x\n", src->pixels[src_now_idx]);
-      dst->pixels[dst_now_idx] = 0;
+      dst_pixels[dst_now_idx] = 0;
       // assert(x == 3);
     }
     // printf("\n");
