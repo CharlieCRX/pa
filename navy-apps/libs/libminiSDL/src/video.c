d23cf130321103fa4579e41a22f6fa58f0472656
tracer-ics2023 <tracer@njuics.org>
2024-12-04 19:07:55 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:07:55 up 14 days,  4:54,  5 users,  load average: 0.17, 0.15, 0.28

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -63,9 +63,9 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
       // printf("src_now_idx = %d\n", src_now_idx);
 
       // 否则正常复制即可
-      dst->pixels[dst_now_idx] = src->pixels[src_now_idx];
+      // dst->pixels[dst_now_idx] = src->pixels[src_now_idx];
       // printf("src->pixels[src_now_idx] = 0x%x\n", src->pixels[src_now_idx]);
-      // dst->pixels[dst_now_idx] = 0;
+      dst->pixels[dst_now_idx] = 0;
       // assert(x == 3);
     }
     // printf("\n");
