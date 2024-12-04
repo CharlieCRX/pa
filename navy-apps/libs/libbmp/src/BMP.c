c9ba675b1c78630ab39f2e01790eeb05c546dc45
tracer-ics2023 <tracer@njuics.org>
2024-12-04 14:02:01 +0800

    >  compile NEMU 17110502001 chenruixin Linux crx-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:02:01 up 13 days, 23:48,  5 users,  load average: 1.02, 0.48, 0.28

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -45,7 +45,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
-
+  fseek(fp, 0, SEEK_SET);
   fclose(fp);
   if (width) *width = w;
   if (height) *height = h;
